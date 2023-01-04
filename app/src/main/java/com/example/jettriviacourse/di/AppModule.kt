c23a12f76bf030b53af33fe4f1d56f59b1ab8791
Dominik Listos <dominlist@gmail.com>
2023-01-04 20:04:16 +0100

    starting setup viewmodel

--- app/src/main/java/com/example/jettriviacourse/di/AppModule.kt
+++ app/src/main/java/com/example/jettriviacourse/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.example.jettriviacourse.di
 
 import com.example.jettriviacourse.network.QuestionApi
+import com.example.jettriviacourse.repository.QuestionRepository
 import com.example.jettriviacourse.util.Constants
 import dagger.Module
 import dagger.Provides
@@ -13,6 +14,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(api: QuestionApi) = QuestionRepository(api)
 
     @Singleton
     @Provides
