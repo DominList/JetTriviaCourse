ed34b6ba9e445931215993bbaef8bea7133e3512
Dominik Listos <dominlist@gmail.com>
2023-01-13 22:23:00 +0100

    connecting ui to data

--- app/src/main/java/com/example/jettriviacourse/component/Questions.kt
+++ app/src/main/java/com/example/jettriviacourse/component/Questions.kt
@@ -1,22 +1,37 @@
 package com.example.jettriviacourse.component
 
-import android.graphics.PathEffect
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -24,33 +39,76 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jettriviacourse.model.QuestionItem
 import com.example.jettriviacourse.screens.QuestionsViewModel
 import com.example.jettriviacourse.util.AppColors
-import java.util.Locale
 
 @Composable
 fun Questions(viewModel: QuestionsViewModel) {
     val questions = viewModel.data.value.data?.toMutableList()
     Log.d("Loading data", "List size: ${questions?.size}")
 
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
+
+    val questionsNumber = remember {
+        mutableStateOf(0)
+    }
+
     if (viewModel.isDataLoading == true) {
         CircularProgressIndicator(modifier = Modifier.size(100.dp))
         Log.d("Loading data", "Questions are loading!")
     } else {
-        questions?.forEachIndexed { index, questionsItem ->
-            val formattedIndex = String.format(Locale.getDefault(), "%4d.", index + 1)
-            Log.d("Loading data", "Questions: $formattedIndex ${questionsItem.question}")
+        val question = try {
+            questions?.get(questionIndex.value)
+        } catch (e: Exception) {
+            null
         }
-
-        //LazyColumn(content = )
+        question?.let {
+            QuestionDisplay(
+                questionItem = question,
+                questionIndex = questionIndex,
+                questionsNumber = questionsNumber,
+                onNextClicked = {
+                    questionIndex.value.inc()
+                }
+            )
+        }
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    questionItem: QuestionItem,
+    questionIndex: MutableState<Int> = mutableStateOf(0),
+    questionsNumber: MutableState<Int> = mutableStateOf(0),
+//    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit = {}
+) {
+    val choicesState = remember(questionItem) {
+        questionItem.choices.toMutableList()
+    }
+
+    val answerState = remember(questionItem) {
+        mutableStateOf<Int?>(null)
+    }
+
+    val correctAnswerState = remember(questionItem) {
+        mutableStateOf<Boolean?>(null)
+    }
+
+    val updateAnswer: (Int) -> Unit = remember(questionItem) {
+        { index ->
+            answerState.value = index
+            correctAnswerState.value = choicesState[index] == questionItem.answer
+        }
+    }
+
     val pathEffect =
-        androidx.compose.ui.graphics.PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+        PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 10f)
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,24 +122,107 @@ fun QuestionDisplay() {
             horizontalAlignment = Alignment.Start
 
         ) {
-            QuestionTracker()
+            QuestionTracker(counter = questionIndex.value, outOf =  questionsNumber.value)
             DrawDottedLine(pathEffect = pathEffect)
             Column {
                 Text(
-                    modifier = Modifier.padding(6.dp).align(alignment = Alignment.Start).fillMaxHeight(0.3f).fillMaxWidth(),
-                    text = "Text of the question?",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f)
+                        .fillMaxWidth(),
+                    text = questionItem.question,
                     fontSize = 17.sp,
                     fontWeight = FontWeight.Bold,
                     lineHeight = 22.sp,
                     color = AppColors.mOffWhite
                 )
+
+                // choices
+                choicesState.forEachIndexed { index, answer ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp,
+                                brush = Brush.linearGradient(
+                                    colors = AppColors.run {
+                                        listOf(
+                                            mOffDarkPurple,
+                                            mBlue,
+                                            mLightGray
+                                        )
+                                    },
+                                ),
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    bottomEndPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomStartPercent = 50
+                                )
+                            )
+                            .background(color = Color.Transparent),
+                        verticalAlignment = CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = answerState.value == index,
+                            onClick = { updateAnswer(index) },
+                            modifier = Modifier.padding(start = 16.dp),
+                            colors = RadioButtonDefaults.colors(
+                                selectedColor = if (correctAnswerState.value == true && index == answerState.value)
+                                    Color.Green.copy(alpha = .7f)
+                                else
+                                    Color.Red.copy(alpha = .7f),
+                                unselectedColor = AppColors.mOffWhite
+                            )
+                        )
+                        val annotatedString = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.Light,
+                                    color = if (correctAnswerState.value == true && index == answerState.value)
+                                        Color.Green.copy(alpha = .7f)
+                                    else if (correctAnswerState.value == false && index == answerState.value)
+                                        Color.Red.copy(alpha = .7f)
+                                    else AppColors.mOffWhite,
+                                    fontSize = 17.sp
+                                )
+                            )
+                            {
+                                append(answer)
+                            }
+                        }
+                        Text(text = annotatedString, modifier = Modifier.padding(6.dp))
+                    }
+                }
+                Button(
+                    onClick = { onNextClicked(questionIndex.value) },
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = AppColors.mLightBlue),
+                ) {
+                    Text(
+                        text = "Next",
+                        modifier = Modifier.padding(4.dp),
+                        color = AppColors.mOffWhite,
+                        fontSize = 17.sp
+                    )
+                }
             }
         }
     }
 }
 
+
 @Composable
-fun DrawDottedLine(pathEffect: androidx.compose.ui.graphics.PathEffect) {
+fun DrawDottedLine(pathEffect: PathEffect) {
     Canvas(
         modifier = Modifier
             .fillMaxWidth()
@@ -108,7 +249,6 @@ fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
                     fontSize = 27.sp
                 )
             ) {
-
                 append("Question $counter/")
                 withStyle(
                     style = SpanStyle(
